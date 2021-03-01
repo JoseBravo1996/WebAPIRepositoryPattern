192d7276e2165a3dff33e421394bbf0029f7da3b
JBravo <bravo.jose.luis18@gmail.com>
2021-03-01 16:16:26 -0300

    Add project files.

---
+++ WebAPIRepositoryPattern/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIRepositoryPattern.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public int FullName { get; set; }
+        public int Department { get; set; }
+        public int JoinDate { get; set; }
+
+
+    }
+}
